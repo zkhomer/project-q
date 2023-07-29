9a6f3ac82415a0cd9df467d91ca826d22ae60e96
Artur <zkhomers@gmail.com>
2023-07-29 15:21:09 +0300

    feat: add animation handler.

--- main.js
+++ main.js
@@ -1,24 +1,89 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const dropdown = document.getElementById('animations')
+let playerState = 'fall'
+dropdown.addEventListener('change', function (e){
+    playerState = e.target.value
+})
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 600
 
-setupCounter(document.querySelector('#counter'))
+
+const playerImage = new Image();
+playerImage.src = './assets/shadow_dog.png'
+
+const spriteWidth = 575
+const spriteHeight = 523
+let gameFrame = 0;
+const staggerFrames = 1;
+const spriteAnimations = [];
+const animationsStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
+    },
+]
+
+animationsStates.forEach((state, index) => {
+    let frames = {
+        loc: []
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY})
+    }
+    spriteAnimations[state.name] = frames;
+})
+
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
+    const frameX = spriteWidth * position
+    const frameY = spriteAnimations[playerState].loc[position].y
+
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+
+    gameFrame++
+    requestAnimationFrame(animate)
+}
+
+animate()
